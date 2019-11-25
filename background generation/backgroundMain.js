88d5e478d580f2e95cee1bd4e287f2d358ece7fc
Jeffe <j.garner200@gmail.com>
2019-11-25 15:15:45 -0500

    started on backgrounds

---
+++ background generation/backgroundMain.js
@@ -0,0 +1,245 @@
+const acolyte= {
+  name: "Acolyte",
+  skillProficiencies: ["", ""],
+  languages: "",
+  equipment: ["", ""],
+  feature: { a: "" },
+  suggestedCharacteristics: [
+    { personalityTrait: ["", "", "", "", "", "", "", "", ""] },
+    { ideal: ["", "", "", "", "", "", ""] },
+    { bond: ["", "", "", "", "", "", ""] },
+    { flaw: ["", "", "", "", "", "", ""] }
+  ],
+  variant: { a: "" }
+};
+const charlatan = {
+  name: "Charlatan",
+  skillProficiencies: ["", ""],
+  languages: "",
+  equipment: ["", ""],
+  feature: { a: "" },
+  suggestedCharacteristics: [
+    { personalityTrait: ["", "", "", "", "", "", "", "", ""] },
+    { ideal: ["", "", "", "", "", "", ""] },
+    { bond: ["", "", "", "", "", "", ""] },
+    { flaw: ["", "", "", "", "", "", ""] }
+  ],
+  variant: { a: "" }
+};
+const criminal = {
+  name: "Criminal",
+  skillProficiencies: ["", ""],
+  languages: "",
+  equipment: ["", ""],
+  feature: { a: "" },
+  suggestedCharacteristics: [
+    { personalityTrait: ["", "", "", "", "", "", "", "", ""] },
+    { ideal: ["", "", "", "", "", "", ""] },
+    { bond: ["", "", "", "", "", "", ""] },
+    { flaw: ["", "", "", "", "", "", ""] }
+  ],
+  variant: { a: "" }
+};
+const entertainer = {
+  name: "Entertainer",
+  skillProficiencies: ["", ""],
+  languages: "",
+  equipment: ["", ""],
+  feature: { a: "" },
+  suggestedCharacteristics: [
+    { personalityTrait: ["", "", "", "", "", "", "", "", ""] },
+    { ideal: ["", "", "", "", "", "", ""] },
+    { bond: ["", "", "", "", "", "", ""] },
+    { flaw: ["", "", "", "", "", "", ""] }
+  ],
+  variant: { a: "" }
+};
+const folkHero = {
+  name: "FolkHero",
+  skillProficiencies: ["", ""],
+  languages: "",
+  equipment: ["", ""],
+  feature: { a: "" },
+  suggestedCharacteristics: [
+    { personalityTrait: ["", "", "", "", "", "", "", "", ""] },
+    { ideal: ["", "", "", "", "", "", ""] },
+    { bond: ["", "", "", "", "", "", ""] },
+    { flaw: ["", "", "", "", "", "", ""] }
+  ],
+  variant: { a: "" }
+};
+const gladiator = {
+  name: "Gladiator",
+  skillProficiencies: ["", ""],
+  languages: "",
+  equipment: ["", ""],
+  feature: { a: "" },
+  suggestedCharacteristics: [
+    { personalityTrait: ["", "", "", "", "", "", "", "", ""] },
+    { ideal: ["", "", "", "", "", "", ""] },
+    { bond: ["", "", "", "", "", "", ""] },
+    { flaw: ["", "", "", "", "", "", ""] }
+  ],
+  variant: { a: "" }
+};
+const guildArtisan = {
+  name: "Guild Artisan",
+  skillProficiencies: ["", ""],
+  languages: "",
+  equipment: ["", ""],
+  feature: { a: "" },
+  suggestedCharacteristics: [
+    { personalityTrait: ["", "", "", "", "", "", "", "", ""] },
+    { ideal: ["", "", "", "", "", "", ""] },
+    { bond: ["", "", "", "", "", "", ""] },
+    { flaw: ["", "", "", "", "", "", ""] }
+  ],
+  variant: { a: "" }
+};
+const hermit = {
+  name: "Hermit",
+  skillProficiencies: ["", ""],
+  languages: "",
+  equipment: ["", ""],
+  feature: { a: "" },
+  suggestedCharacteristics: [
+    { personalityTrait: ["", "", "", "", "", "", "", "", ""] },
+    { ideal: ["", "", "", "", "", "", ""] },
+    { bond: ["", "", "", "", "", "", ""] },
+    { flaw: ["", "", "", "", "", "", ""] }
+  ],
+  variant: { a: "" }
+};
+const knight = {
+  name: "Knight",
+  skillProficiencies: ["", ""],
+  languages: "",
+  equipment: ["", ""],
+  feature: { a: "" },
+  suggestedCharacteristics: [
+    { personalityTrait: ["", "", "", "", "", "", "", "", ""] },
+    { ideal: ["", "", "", "", "", "", ""] },
+    { bond: ["", "", "", "", "", "", ""] },
+    { flaw: ["", "", "", "", "", "", ""] }
+  ],
+  variant: { a: "" }
+};
+const noble = {
+  name: "Noble",
+  skillProficiencies: ["", ""],
+  languages: "",
+  equipment: ["", ""],
+  feature: { a: "" },
+  suggestedCharacteristics: [
+    { personalityTrait: ["", "", "", "", "", "", "", "", ""] },
+    { ideal: ["", "", "", "", "", "", ""] },
+    { bond: ["", "", "", "", "", "", ""] },
+    { flaw: ["", "", "", "", "", "", ""] }
+  ],
+  variant: { a: "" }
+};
+const outlander = {
+  name: "Outlander",
+  skillProficiencies: ["", ""],
+  languages: "",
+  equipment: ["", ""],
+  feature: { a: "" },
+  suggestedCharacteristics: [
+    { personalityTrait: ["", "", "", "", "", "", "", "", ""] },
+    { ideal: ["", "", "", "", "", "", ""] },
+    { bond: ["", "", "", "", "", "", ""] },
+    { flaw: ["", "", "", "", "", "", ""] }
+  ],
+  variant: { a: "" }
+};
+const pirate = {
+  name: "Pirate",
+  skillProficiencies: ["", ""],
+  languages: "",
+  equipment: ["", ""],
+  feature: { a: "" },
+  suggestedCharacteristics: [
+    { personalityTrait: ["", "", "", "", "", "", "", "", ""] },
+    { ideal: ["", "", "", "", "", "", ""] },
+    { bond: ["", "", "", "", "", "", ""] },
+    { flaw: ["", "", "", "", "", "", ""] }
+  ],
+  variant: { a: "" }
+};
+const sage = {
+  name: "Sage",
+  skillProficiencies: ["", ""],
+  languages: "",
+  equipment: ["", ""],
+  feature: { a: "" },
+  suggestedCharacteristics: [
+    { personalityTrait: ["", "", "", "", "", "", "", "", ""] },
+    { ideal: ["", "", "", "", "", "", ""] },
+    { bond: ["", "", "", "", "", "", ""] },
+    { flaw: ["", "", "", "", "", "", ""] }
+  ],
+  variant: { a: "" }
+};
+const sailor = {
+  name: "Sailor",
+  skillProficiencies: ["", ""],
+  languages: "",
+  equipment: ["", ""],
+  feature: { a: "" },
+  suggestedCharacteristics: [
+    { personalityTrait: ["", "", "", "", "", "", "", "", ""] },
+    { ideal: ["", "", "", "", "", "", ""] },
+    { bond: ["", "", "", "", "", "", ""] },
+    { flaw: ["", "", "", "", "", "", ""] }
+  ],
+  variant: { a: "" }
+};
+const soldier = {
+  name: "Soldier",
+  skillProficiencies: ["", ""],
+  languages: "",
+  equipment: ["", ""],
+  feature: { a: "" },
+  suggestedCharacteristics: [
+    { personalityTrait: ["", "", "", "", "", "", "", "", ""] },
+    { ideal: ["", "", "", "", "", "", ""] },
+    { bond: ["", "", "", "", "", "", ""] },
+    { flaw: ["", "", "", "", "", "", ""] }
+  ],
+  variant: { a: "" }
+};
+const urchin = {
+  name: "Urchin",
+  skillProficiencies: ["", ""],
+  languages: "",
+  equipment: ["", ""],
+  feature: { a: "" },
+  suggestedCharacteristics: [
+    { personalityTrait: ["", "", "", "", "", "", "", "", ""] },
+    { ideal: ["", "", "", "", "", "", ""] },
+    { bond: ["", "", "", "", "", "", ""] },
+    { flaw: ["", "", "", "", "", "", ""] }
+  ],
+  variant: { a: "" }
+};
+
+const background = {
+    acolyte:acolyte,
+    charlatan:charlatan,
+    criminal:criminal,
+    entertainer:entertainer,
+    folkHero:folkHero,
+    gladiator:gladiator,
+    guildArtisan:guildArtisan,
+    hermit:hermit,
+    knight:knight,
+    noble:noble,
+    outlander:outlander,
+    pirate:pirate,
+    sage:sage,
+    sailor:sailor,
+    soldier:soldier,
+    urchin:urchin,
+}
+
+module.exports = background
