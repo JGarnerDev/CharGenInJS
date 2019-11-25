8281c58c12c82c7a5b1c456cc74de37ba093faa3
Jeffe <j.garner200@gmail.com>
2019-11-25 00:33:06 -0500

    -

--- main/characterGen.js
+++ main/characterGen.js
@@ -3,19 +3,13 @@ var statsRoll = require("../stats generation/statsRoll");
 //      and remove the x smallest dice rolls before returning the sum. If no argument is passed, each stat will be the sum of 3 rolls
 //      of six-sided dice.
 
-var classes = require("../class generation/classMain")
+var classes = require("../class generation/classMain");
 // character class (object; a collection of properties associated with a randomly selected class)
 
-let charStats = statsRoll() 
+let charStats = statsRoll();
 
+let charClass = classes[Math.floor(Math.random() * 12)];
 
-let charClass = classes[Math.floor(Math.random()*12)]
+let character = [charStats, charClass];
 
-
-console.log(charStats);
-
-
-
-
-
-
+console.log("Stats: " + character[0].join(" ") + "       Class: " + character[1].name + " (" + character[1].description + ")")
